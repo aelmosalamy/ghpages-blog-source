d732877621438c26eace0ca4f0416952559ecf7c
aelmosalamy <adham@elmosalamy.net>
2019-06-07 23:55:58 +0400

    Minor changes

---
+++ content/posts/testing-website.md
@@ -0,0 +1,6 @@
+Title: Testing Website!
+Date: 07-06-2019 23:44
+Tags: new, edit
+Slug: testing-website
+
+*This is a placeholder, place your content here.*
--- output-to-ghpages.py
+++ output-to-ghpages.py
@@ -1,19 +1,21 @@
 from distutils.dir_util import copy_tree
+from shutil import rmtree
 import os
 
 # if specified all files will be deleted - except .git - before copying
 # otherwise copying will add files if non-existing and overwrite files
 # which exists
-CLEANUP_BEFORE_COPYING = False
+CLEANUP_BEFORE_COPYING = True
 
 # runs pelican to convert "content" into "output"
 os.system("pelican")
-print("'pelican' called!")
+print("Pelican site generated to output!")
 
 if CLEANUP_BEFORE_COPYING:
     for filename in os.listdir("gh-pages"):
-        if filename != ".git"
-            os.remove(filename)
+        if filename != ".git":
+            path = "gh-pages/" + filename
+            os.remove(path) if os.path.isfile(path) else rmtree(path)
         else:
             pass
     print("Clean up completed!")
