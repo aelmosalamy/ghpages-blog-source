40e03205dcb4e8d3ef96dbb862fd3a212cefdbbe
aelmosalamy <adham@elmosalamy.net>
2019-06-24 21:16:48 +0400

    Support for inline tags

---
+++ publishsite.py
@@ -0,0 +1,26 @@
+from distutils.dir_util import copy_tree
+from shutil import rmtree
+import os
+
+# if specified all files will be deleted - except .git - before copying
+# otherwise copying will add files if non-existing and overwrite files
+# which exists
+CLEANUP_BEFORE_COPYING = True
+
+# runs pelican to convert "content" into "output"
+os.system("make publish")
+print("Pelican site published!")
+
+if CLEANUP_BEFORE_COPYING:
+    for filename in os.listdir("gh-pages"):
+        if filename != ".git":
+            path = "gh-pages/" + filename
+            os.remove(path) if os.path.isfile(path) else rmtree(path)
+        else:
+            pass
+    print("Clean up completed!")
+    copy_tree("output/", "gh-pages/")
+    print("Tree fully copied!")
+else:
+    copy_tree("output/", "gh-pages/")
+    print("Tree fully copied!")
